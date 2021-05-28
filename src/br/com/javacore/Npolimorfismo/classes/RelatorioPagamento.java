4c7f3b48ea9131e906a00c24eff7671af48957c3
suprnovacoins <suprnovacoins@gmail.com>
2021-05-28 14:07:05 -0300

    Polimorfismo parte_2.

--- src/br/com/javacore/Npolimorfismo/classes/RelatorioPagamento.java
+++ src/br/com/javacore/Npolimorfismo/classes/RelatorioPagamento.java
@@ -2,23 +2,30 @@ package br.com.javacore.Npolimorfismo.classes;
 
 public class RelatorioPagamento {
 
-    public void relatorioPagamentoGerente(Gerente gerente){
-        System.out.println("Gerando relatorio de pagamento para genrencia");
-        gerente.calculaPagamento();
-        System.out.println("Nome: "+ gerente.getNome());
-        System.out.println("Salario desse mes: "+ gerente.getSalario());
-    }
-    public void relatorioPagamentoVendedor(Vendedor vendedor){
-        System.out.println("Gerando relatorio de pagamento dos vendedores");
-        vendedor.calculaPagamento();
-        System.out.println("Nome: "+ vendedor.getNome());
-        System.out.println("Salario desse mes: "+ vendedor.getSalario());
-    }
+//    public void relatorioPagamentoGerente(Gerente gerente){
+//        System.out.println("Gerando relatorio de pagamento para genrencia");
+//        gerente.calculaPagamento();
+//        System.out.println("Nome: "+ gerente.getNome());
+//        System.out.println("Salario desse mes: "+ gerente.getSalario());
+//    }
+//    public void relatorioPagamentoVendedor(Vendedor vendedor){
+//        System.out.println("Gerando relatorio de pagamento dos vendedores");
+//        vendedor.calculaPagamento();
+//        System.out.println("Nome: "+ vendedor.getNome());
+//        System.out.println("Salario desse mes: "+ vendedor.getSalario());
+//    }
 
-    public void relatorioPagamentoGenerico(Funcionario funcionario){
+    public void relatorioPagamentoGenerico(Funcionario funcionario) {
         System.out.println("Gerando relatorio de pagamento");
         funcionario.calculaPagamento();
-        System.out.println("Nome: "+funcionario.getNome());
-        System.out.println("Salario mês: "+funcionario.getSalario());
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("Salario mês: " + funcionario.getSalario());
+        if (funcionario instanceof Gerente) {
+            Gerente g = (Gerente) funcionario;
+            System.out.println("Participacao nos lucros: " + g.getPnl());
+        }
+        if (funcionario instanceof Vendedor) {
+            System.out.println("Participacao nos lucros: " + ((Vendedor) funcionario).getTotalVendas());
+        }
     }
 }
--- src/br/com/javacore/Npolimorfismo/test/PolimorfismoTest.java
+++ src/br/com/javacore/Npolimorfismo/test/PolimorfismoTest.java
@@ -14,7 +14,7 @@ public class PolimorfismoTest {
 //        relatorio.relatorioPagamentoVendedor(v);
 
         relatorio.relatorioPagamentoGenerico(g);
-        System.out.println("------------------------");
-        relatorio.relatorioPagamentoGenerico(v);
+//        System.out.println("------------------------");
+//        relatorio.relatorioPagamentoGenerico(v);
     }
 }
