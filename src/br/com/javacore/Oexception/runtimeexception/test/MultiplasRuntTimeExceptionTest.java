a0c66fe90f0341ade475ca19d0735ae03e6f297a
suprnovacoins <suprnovacoins@gmail.com>
2021-05-29 15:36:20 -0300

    Exceptions parte_7.

--- src/br/com/javacore/Oexception/runtimeexception/test/MultiplasRuntTimeExceptionTest.java
+++ src/br/com/javacore/Oexception/runtimeexception/test/MultiplasRuntTimeExceptionTest.java
@@ -1,32 +1,26 @@
 package br.com.javacore.Oexception.runtimeexception.test;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class MultiplasRuntTimeExceptionTest {
     public static void main(String[] args) {
-        try{
+        try {
             throw new IllegalArgumentException();
-        }catch (ArrayIndexOutOfBoundsException a){
+        } catch (ArithmeticException | IllegalArgumentException | ArrayIndexOutOfBoundsException a) {
             System.out.println("Dentro de ArrayIndexOutOfBoundsException");
-        }catch (IllegalArgumentException i){
-            System.out.println("Dentro de IllegalArgumentException");
-        }catch (ArithmeticException ari){
-            System.out.println("Dentro de ArithmeticException");
-        }catch (RuntimeException ari){
-            System.out.println("Dentro de RuntimeException");
         }
         System.out.println("Final do programa");
 
         try {
             talvezLanceException();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
         }
     }
-    private static void talvezLanceException() throws SQLException, FileNotFoundException{
+
+    private static void talvezLanceException() throws SQLException, AWTException, IOException {
 
     }
 }
