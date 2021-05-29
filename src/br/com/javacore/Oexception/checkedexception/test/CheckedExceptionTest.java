2ceb825277db0734b309ca91b4f6920fc208b16e
suprnovacoins <suprnovacoins@gmail.com>
2021-05-29 11:44:41 -0300

    Exceptions parte_4.

--- src/br/com/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
+++ src/br/com/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
@@ -1,19 +1,25 @@
 package br.com.javacore.Oexception.checkedexception.test;
 
 import java.io.File;
+import java.io.IOException;
 
 public class CheckedExceptionTest {
-    public static void main(String[] args) {
-        criarArquivo();
+    public static void main(String[] args){
+        try {
+            criarArquivo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void criarArquivo() {
+    public static void criarArquivo() throws IOException {
         File file = new File("Texto.txt");
         try {
             System.out.println("Arquivo criado: " + file.createNewFile());
         } catch (Exception e) {
             System.out.println("Arquivo nao foi criado, talvez ja exista um com o mesmo nome");
             e.printStackTrace();
+            throw e;
         }
     }
 }
--- src/br/com/javacore/Oexception/runtimeexception/test/RunTimeExceptionTest.java
+++ src/br/com/javacore/Oexception/runtimeexception/test/RunTimeExceptionTest.java
@@ -3,23 +3,20 @@ package br.com.javacore.Oexception.runtimeexception.test;
 import java.io.IOException;
 
 public class RunTimeExceptionTest {
-    public static void main(String[] args) {
-//       Object o = null;
-//        int a = 10;
-//        int b = 0;
-//        if(b != 0) {
-//            System.out.println(a / b);
+    public static void main(String[] args) throws IOException {
+//        try {
+            divisao(10, 0);
+//        } catch (IOException e) {
+//            e.printStackTrace();
 //        }
-//
-//        System.out.println(o.toString());
+    }
 
-        try {
-            int[] w = new int[2];
-            System.out.println(w[2]);
-            System.out.println("Imprimindo depois da possibvel execucao");
-        }catch (RuntimeException e){
-            e.printStackTrace();
-        }
-        System.out.println("Fora do bloco catch");
-        }
+    private static void divisao(double num1, int num2) {
+//        if(num2==0){
+////            throw new ArithmeticException("Nao é possivel divisao por 0");
+//            throw new IllegalStateException("Nao é possivel divisao por 0");
+//        }
+        double result = num1 / num2;
+        System.out.println(result);
+    }
 }
