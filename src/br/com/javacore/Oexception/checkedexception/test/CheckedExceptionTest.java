246f8fc8669d85a9af9affec5a8d3e7761ac492c
suprnovacoins <suprnovacoins@gmail.com>
2021-05-29 12:06:01 -0300

    Exceptions parte_5.

--- src/br/com/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
+++ src/br/com/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
@@ -5,21 +5,39 @@ import java.io.IOException;
 
 public class CheckedExceptionTest {
     public static void main(String[] args){
-        try {
-            criarArquivo();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        abrirArquivo();
+//        try {
+//            criarArquivo();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public static void criarArquivo() throws IOException {
         File file = new File("Texto.txt");
         try {
-            System.out.println("Arquivo criado: " + file.createNewFile());
+            file.createNewFile();
+            System.out.println("Arquivo criado");
         } catch (Exception e) {
             System.out.println("Arquivo nao foi criado, talvez ja exista um com o mesmo nome");
             e.printStackTrace();
             throw e;
         }
+    }
+
+    public static String abrirArquivo() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Executando leitura do arquivo");
+//            throw new Exception();
+            System.out.println("Escrevendo no arquivo");
+            return "Valor";
+        } catch (Exception e) {
+            System.out.println("Dentro do catch");
+            e.printStackTrace();
+        }finally {
+            System.out.println("Fechar arquivo");
+        }
+        return null;
     }
 }
