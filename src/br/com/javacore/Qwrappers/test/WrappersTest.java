727118aaaae34c7719253c87285f29c9608e7f37
suprnovacoins <suprnovacoins@gmail.com>
2021-05-31 12:13:44 -0300

    Wrappers.

---
+++ src/br/com/javacore/Qwrappers/test/WrappersTest.java
@@ -0,0 +1,23 @@
+package br.com.javacore.Qwrappers.test;
+
+public class WrappersTest {
+    public static void main(String[] args) {
+        byte bytePrimitivo = 1;
+        short shortPrimitivo = 1;
+        int intPrimitivo = 10;
+        long longPrimitivo = 10L;
+        float floatPrimitivo = 10F;
+        double doublePrimitivo = 10D;
+        char charPrimitivo = 'A';
+        boolean booleanPrimitivo = true;
+
+        Byte byteWrapper = 1;
+        Short shortWrapper = 1;
+        Integer integerWrapper = 1;
+        Long longWrapper = 10L;
+        Float floatWrapper = 10F;
+        Double doubleWrapper = 10D;
+        Character characterWrapper = 'A';
+        Boolean booleanWrapper = true;
+    }
+}
