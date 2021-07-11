b8bcd1b50f1c76fad0b485b601de002b2241976a
suprnovacoins <suprnovacoins@gmail.com>
2021-07-11 11:54:55 -0300

    nio parte_04

---
+++ src/br/com/javacore/Xnio/test/resolvendoPathsTest.java
@@ -0,0 +1,25 @@
+package br.com.javacore.Xnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class resolvendoPathsTest {
+    public static void main(String[] args) {
+        Path dir = Paths.get("pasta\\subpasta");
+        Path arquivo = Paths.get("subsubpasta\\file.txt");
+        Path result = dir.resolve(arquivo);
+        System.out.println(result);
+
+        Path absoluto = Paths.get("pasta\\subpasta");
+        Path relativo = Paths.get("subsubpasta");
+        Path file = Paths.get("file.txt");
+
+        System.out.println("1: "+absoluto.resolve(relativo));
+        System.out.println("2: "+absoluto.resolve(file));
+        System.out.println("3: "+relativo.resolve(file));
+        System.out.println("4: "+relativo.resolve(absoluto));
+        System.out.println("5: "+file.resolve(absoluto));
+        System.out.println("6: "+file.resolve(relativo));
+
+    }
+}
