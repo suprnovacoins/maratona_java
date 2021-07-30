f6dce6b8922505e4c84a27aa5e26d66cd7b93a07
suprnovacoins <suprnovacoins@gmail.com>
2021-07-30 15:16:50 -0300

    Colecoes parte_07

--- src/br/com/javacore/Zcolecoes/test/SortProdutoTest.java
+++ src/br/com/javacore/Zcolecoes/test/SortProdutoTest.java
@@ -2,13 +2,21 @@ package br.com.javacore.Zcolecoes.test;
 
 import br.com.javacore.Zcolecoes.classes.Produto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+
+class ProdutoNomeComparator implements Comparator<Produto>{
+
+    @Override
+    public int compare(Produto o1, Produto o2) {
+        return o1.getNome().compareTo(o2.getNome());
+    }
+}
 public class SortProdutoTest {
     public static void main(String[] args) {
         List<Produto> produtos = new ArrayList<>();
+        Produto[] produtosArray = new Produto[4];
+
         Produto produto1 = new Produto("123","Lenovo Notebook", 3500.00);
         Produto produto2 = new Produto("321","Picanha", 20.00);
         Produto produto3 = new Produto("879","Teclado Razer", 1000.00);
@@ -17,9 +25,21 @@ public class SortProdutoTest {
         produtos.add(produto2);
         produtos.add(produto3);
         produtos.add(produto4);
-        Collections.sort(produtos);
-        for (Produto produto : produtos){
+
+        produtosArray[0] = produto1;
+        produtosArray[1] = produto2;
+        produtosArray[2] = produto3;
+        produtosArray[3] = produto4;
+
+        Collections.sort(produtos, new ProdutoNomeComparator());
+//        for (Produto produto : produtos){
+//            System.out.println(produto);
+//        }
+
+        Arrays.sort(produtosArray,new ProdutoNomeComparator());
+        for (Produto produto : produtosArray){
             System.out.println(produto);
         }
+
     }
 }
