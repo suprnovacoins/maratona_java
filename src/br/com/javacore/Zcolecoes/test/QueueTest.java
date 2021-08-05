6435dd29e3a8a020568cb5b2837539f6470148b2
suprnovacoins <suprnovacoins@gmail.com>
2021-08-04 21:26:38 -0300

    Colecoes parte_16

---
+++ src/br/com/javacore/Zcolecoes/test/QueueTest.java
@@ -0,0 +1,16 @@
+package br.com.javacore.Zcolecoes.test;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+        Queue<String> queue = new PriorityQueue<>();
+        queue.add("C");
+        queue.add("D");
+        queue.add("A");
+        queue.add("B");
+        System.out.println(queue.peek());
+        System.out.println(queue.size());
+    }
+}
